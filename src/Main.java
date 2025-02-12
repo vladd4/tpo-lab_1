7b9508b930463c4f72742af8814906ce82029bfc
Vladyslav D <donetsvlad444@gmail.com>
2025-02-12 12:38:34 +0200

    initial commit lab1_3

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,5 @@ public class Main {
         BounceFrame frame = new BounceFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        System.out.println("Thread name = " + Thread.currentThread().getName());
     }
 }
